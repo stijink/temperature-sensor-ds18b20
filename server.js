87af671534e5ebf44d800790a634af296dea2339
Stephan Tijink <stijink@googlemail.com>
2017-05-17 16:40:07 +0200

    Temperature Sensor http server

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
-const sensor = require('ds18b20-raspi');
-const http   = require('http');
-const port   = 9321;
+const sensor    = require('ds18b20-raspi');
+const http      = require('http');
+const accesslog = require('access-log');
+const port      = 9321;
+
+console.log('started temperature sensor server at port ' + port);
 
 http.createServer(function(request, response) {
 
-  response
-    .writeHead(200)
-    .write('bla')
-    .end();
+  var temperature  = sensor.readSimpleC(2);
+
+  response.writeHead(200);
+  response.write(temperature.toString());
+
+  accesslog(request, response);
+  response.end();
 
 }).listen(port);
